b7f8e3eeca2d704f9816393781c5992df9f9d8a4
cristian <cr.munoz20@gmail.com>
2022-06-19 18:10:28 -0400

    Modificaciones en el css y js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 function calcular(){
-    let qty = document.getElementById("quantity").value;
-    let prc = document.getElementById("price").getAttribute("value");
-    let clr = document.getElementById("color").value;
+    var qty = document.getElementById("quantity").value;
+    var prc = document.getElementById("price").getAttribute("value");
+    var clr = document.getElementById("color").value;
     parseInt(quantity);
     parseInt(price);
-    let total = qty*prc
+    var total = qty*prc
     document.getElementById("total").innerHTML = total.toLocaleString("es-CL", {style: "currency", currency: "CLP"})
     document.getElementById("cantidad").innerHTML = qty;
     document.getElementById("box-color").style.backgroundColor = clr;
